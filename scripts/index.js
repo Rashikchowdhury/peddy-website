6df7871770e94c23f9e92eca6a7293c6400530a7
Rashikchowdhury <muhammadrashikchowdhury@gmail.com>
2025-05-24 19:59:45 +0600

    added the btn clicked styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,45 @@
 // fetch categories data
-const categoriesData = async() => {
-    try{
+const categoriesData = async () => {
+    try {
         let res = await fetch(`https://openapi.programming-hero.com/api/peddy/categories`);
         let data = await res.json();
-        console.log(data);
+        loadCategoryBtn(data.categories);
     }
-    catch(err){
+    catch (err) {
         console.error(err);
     }
 };
 
 const categoryBtn = () => {
     const categoryBtnContainer = document.getElementById("category-btn-container");
-    
+
+};
+categoriesData();
+
+// category buttons
+const loadCategoryBtn = (categories) => {
+    const categoriesContainer = document.getElementById("categories-container");
+
+    categories.forEach(item => {
+        let categoryBtnContainer = document.createElement('div');
+
+        categoryBtnContainer.innerHTML = `
+            <button id="${item.id}" onclick="changeBtnStyle(${item.id})" class="active btn btn-lg flex gap-2 text-2xl font-bold bg-white border border-pri-clr hover:bg-white hover:border-pri-clr">
+                <img src="${item.category_icon}" class="w-8"/>
+                ${item.category}
+            </button>
+        `;
+
+        categoriesContainer.append(categoryBtnContainer);
+    })
 }
 
-categoriesData();
+// change category btn style on click
+const changeBtnStyle = (id) => {
+    const buttons = document.getElementsByClassName("active");
+    
+    for(let btn of buttons){
+        btn.classList.remove("round-btn")
+    }
+    document.getElementById(id).classList.add("round-btn");
+}
